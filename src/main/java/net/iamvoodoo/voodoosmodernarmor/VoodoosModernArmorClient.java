e5b6fc44df359b26a9ac3bc8ad3d1376d8e4fab6
VoodooLikesCoding <roozendaaljonas@gmail.com>
2024-11-02 06:42:00 +0100

    Initial commit

---
+++ src/main/java/net/iamvoodoo/voodoosmodernarmor/VoodoosModernArmorClient.java
@@ -0,0 +1,10 @@
+package net.iamvoodoo.voodoosmodernarmor;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VoodoosModernArmorClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/iamvoodoo/voodoosmodernarmor/VoodoosModernArmorDataGenerator.java
@@ -0,0 +1,11 @@
+package net.iamvoodoo.voodoosmodernarmor;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VoodoosModernArmorDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
